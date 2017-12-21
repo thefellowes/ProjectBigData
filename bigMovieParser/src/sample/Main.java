1c17695ad2da9cd6a5956ea0590df0089fac35c7
thefellowes <marcogeertsma@gmail.com>
2017-12-21 12:18:41 +0100

    Started working on the parser
    TODO :
    -   Still need to get the file path dynamic.
    -   Need to finish the regex lines
    -   Need to be able to implement the regex line correctly

---
+++ bigMovieParser/src/sample/Main.java
@@ -0,0 +1,111 @@
+package sample;
+
+import com.opencsv.CSVWriter;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("Which list would you like to export to CSV?");
+
+        // Constants
+        String IMDBpath = "DatabaseIMDB/"; // TODO: !!
+
+        // Init the readers
+        Scanner s = new Scanner(System.in);
+        String choice = s.next().toLowerCase();
+        FileReader fr = new FileReader(IMDBpath + choice.toLowerCase() + ".list");
+        BufferedReader br = new BufferedReader(fr);
+
+        // Variables
+        String pattern;
+        String substitution;
+        String[] header;
+
+        // Loading the patterns for parsing into .CSV files
+        switch (choice)
+        {
+            case "countries":
+                pattern = "";   //"\W(.*?)\"?\\s\\((\\d{4}|\\?\\?\\?\\?|\\d{4}\\/.*)\\)\\s*(\\((.*)\\))?\\s*(\\{([^\\{}]*)\\})?\\s(\\{\\{(SUSPENDED)\\}\\})?\\s*(.*)";
+                substitution = "$1-$2-$4-$6-$8-$9";
+                header = new String[]{};
+                break;
+            case "movies":
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                break;
+            case "series":
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                break;
+            case "actors":
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                break;
+            case "actresses":
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                break;
+            case "directors":
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                break;
+            case "producers":
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                break;
+            case "ratings":
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                break;
+            case "running-times":
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                break;
+            default:
+                pattern = "";
+                substitution = "";
+                header = new String[]{};
+                System.out.println("List not found!");
+                System.exit(0);
+                break;
+        }
+
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter(IMDBpath + "CSV/" + choice.toLowerCase() + ".csv")))
+        {
+            int count = 0; // TODO: Remove count, in de huidige situatie doorloopt hij alleen de eerste 150 regels van het plaintext bestand.
+
+            csvWriter.writeNext(header); // TODO: Nodig?
+
+            while (br.readLine() != null && count < 150)
+            {
+                String nextLine = br.readLine();
+
+                Pattern r = Pattern.compile(pattern);
+
+                Matcher matcher = r.matcher(nextLine);
+
+                String result = matcher.replaceAll(substitution);
+
+                String[] line = result.split("-");
+
+                csvWriter.writeNext(line, true);
+
+                count++;
+            }
+        }
+    }
+}
