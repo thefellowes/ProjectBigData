58e4f52c146f7f945ee3b4a9781ae07d2e3409ed
Schu1609 <schu1609@student.nhl.nl>
2018-01-16 15:04:36 +0100

    Leonparser aangepast

--- bigMovieParser/src/sample/Main.java
+++ bigMovieParser/src/sample/Main.java
@@ -2,7 +2,10 @@ package sample;
 
 import com.opencsv.CSVWriter;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
