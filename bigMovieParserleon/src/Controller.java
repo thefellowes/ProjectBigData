8db3a62471a1215f42fc1534dfe670bdc5d8e206
Schu1609 <schu1609@student.nhl.nl>
2018-01-21 20:03:39 +0100

    Mijn eigen BigmovieParserleon kun je op dit moment countries in je database zetten en producersparsen leest nu heel producers.list en maakt er goeie regels van die nog in een csv moeten worden veranderd.

--- bigMovieParserleon/src/Controller.java
+++ bigMovieParserleon/src/Controller.java
@@ -1,19 +1,108 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.sql.*;
 
 public class Controller {
+    //        System.out.println("Hello World!");
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/discordbot";
+    static final String USER = "root";
+    static final String PASS = "";
+
+    static public String removeCrap(String line)
+    {
+        line = line.replace("\"", "");
+        line = line.replace("'", "");
+        line = line.replace("#", "");
+        line = line.replace("$", "");
+        line = line.replace("*", "");
+        return line;
+    }
 
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-//        System.out.println("Hello World!");
-//        String naam = in.nextLine();
-//        System.out.println("hallo " + naam + ". hoe gaat het met jou?");
-        string str = "123nogeenswatlol";
-        Pattern pattern = Pattern.compile(" \"\\W(.+?)\"\\s(([A-Za-z0-9?I/_.]+?))(?(?=\\s{{)\\s{{(.+?)}})(?(?=\\s{)\\s{(.+?)\\s(#(.+?))}\\s(.+)|\\s(.+))");
-        Matcher matcher = pattern.matcher(str);
+        Connection conn = null;
+        Statement stmt = null;
+        String csvFile = "/Bigdata/csv/countries.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+        int count = 1;
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Database connected successfully...");
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                String line_filt = removeCrap(line);
+                String[] plines = line_filt.split(cvsSplitBy);
+                if (plines.length == 3)
+                {
+                    for (int i = 0; i < plines.length; i++)
+                        plines[i] = plines[i].trim();
+
+                    stmt = conn.createStatement();
+                    int year = -1;
+                    try {
+                        year = Integer.parseInt(plines[1]);
+                    }
+                    catch (NumberFormatException e) {}
+
+                    if (year != -1) {
+                        try {
+                            String sql = "INSERT INTO countries " +
+                                    "VALUES (" + count + ", '" + plines[0] + "', " + plines[1] + ", '" + plines[2] + "')";
+                            stmt.executeUpdate(sql);
+                            count++;
+                        }
+                        catch (SQLException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se) {
+            }// the database is corrupt
+            try {
+                if (conn != null)
+                    conn.close();
+            }
+            catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }
+        System.out.println("Goodbye!");
     }
 }
+//                    String print = count + ".countries [";
+//                    for (int i = 0; i < plines.length; i++)
+//                    {
+//                        if (plines[i].isEmpty() || plines[i] == null)
+//                            plines[i] = "niks";
+//                        print += " line" + i + "=" + plines[i];
+//                        //print += plines[i];
+//                    }
+//                    System.out.println(print + "]");
